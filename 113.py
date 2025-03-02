300140db9216a2df3ebb9b81223f9a0aaabf78a5
Michael Focshaner <michaelfocshaner3@gmail.com>
2025-03-02 20:36:04 +0200

    113: reformat.

--- 113.py
+++ 113.py
@@ -1,31 +1,34 @@
 import numpy as np
 
+
 def get_num_inc(k):
-  mat = np.zeros((k, 9))
-  for i in range(k):
-    for j in range(9):
-      if i == 0:
-        mat[i][j] = 9 - j
-      else:
-        for l in range(j, 9):
-          mat[i][j] += mat[i-1][l]
-  return mat
+    mat = np.zeros((k, 9))
+    for i in range(k):
+        for j in range(9):
+            if i == 0:
+                mat[i][j] = 9 - j
+            else:
+                for digit in range(j, 9):
+                    mat[i][j] += mat[i - 1][digit]
+    return mat
 
 
 my_mat = get_num_inc(6)
-sum = my_mat[5].sum()
-print(sum)
+my_sum = my_mat[5].sum()
+print(my_sum)
+
 
 def get_num_dec(k):
-  mat = np.zeros((k, 9))
-  for i in range(k):
-    for j in range(9):
-      if i == 0:
-        mat[i][j] = 10 - j
-      else:
-        for l in range(j, 9):
-          mat[i][j] += mat[i-1][l]
-  return mat
+    mat = np.zeros((k, 9))
+    for i in range(k):
+        for j in range(9):
+            if i == 0:
+                mat[i][j] = 10 - j
+            else:
+                for digit in range(j, 9):
+                    mat[i][j] += mat[i - 1][digit]
+    return mat
+
 
 my_mat = get_num_dec(3)
 print(my_mat)
